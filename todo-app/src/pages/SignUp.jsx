5001a0a12bc52d09c3e5fdb78f5d48e3898e34ab
Umesh Yadav <umeshyadavrocks@gmail.com>
2022-05-13 19:48:46 +0530

    Getting Started

---
+++ todo-app/src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <>
+        <div>
+        <h2>Register To Get Started....</h2>
+            </div> 
+    </>
+  )
+}
